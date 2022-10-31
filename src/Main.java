d3928a8dd84fb3188d6239f3d82be1712317e6dc
User1 <aglaiagrushkina@gmail.com>
2022-10-31 17:20:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int k = 0;
+        while (k < 11) {
+            System.out.println("My number  " + (k*3));
+            k++;
+        }
+
+
+        int sum = 0;
+        for(int i = 1; i <= 100; i++)
+        { sum = sum + i; }
+        System. out. println("Sum of first 100 numbers is : " + sum);
+        System.out.println("Result"+ sum / 100);
+
+
+
+
+
+
+
+
+    }
+}
